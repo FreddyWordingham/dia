d0c81cc934618d7cd6659ccc7493e931e7ec1719
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-04-20 19:29:22 +0100

    Added library core file.

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+//! Library core.
+
+#![warn(
+    clippy::all,
+    clippy::cargo,
+    clippy::missing_docs_in_private_items,
+    clippy::nursery,
+    clippy::pedantic,
+    clippy::restriction
+)]
+#![allow(
+    clippy::implicit_return
+)]
