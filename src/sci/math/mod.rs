8ca43604f8d96d0671f9325b8ef365a02287c7e2
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-04-21 18:42:50 +0100

    Added linalg aliases.

--- src/sci/math/mod.rs
+++ src/sci/math/mod.rs
@@ -3,3 +3,14 @@
 pub mod sample;
 
 pub use self::sample::*;
+
+use nalgebra::{Point3, Unit, Vector3};
+
+/// Three-dimensional vector alias.
+pub type Vec3 = Vector3<f64>;
+
+/// Normalised three-dimensional vector alias.
+pub type Dir3 = Unit<Vector3<f64>>;
+
+/// Three-dimensional position alias.
+pub type Pos3 = Point3<f64>;
