0221ec3a39ce130318959e179b44b9dee65c4657
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-04-22 16:55:41 +0100

    Added mesh surface.

---
+++ src/geom/surf/mesh.rs
@@ -0,0 +1,65 @@
+//! Smooth triangle-mesh implementation.
+
+use crate::{access, Aabb, Cartesian::X, Greek::Alpha, SmoothTriangle};
+
+/// Mesh geometry.
+pub struct Mesh {
+    /// Bounding box.
+    aabb: Aabb,
+    /// List of component triangles.
+    tris: Vec<SmoothTriangle>,
+}
+
+impl Mesh {
+    access!(aabb, Aabb);
+    access!(tris, Vec<SmoothTriangle>);
+
+    /// Construct a new instance.
+    #[inline]
+    #[must_use]
+    pub fn new(tris: Vec<SmoothTriangle>) -> Self {
+        Self {
+            aabb: Self::init_aabb(&tris),
+            tris,
+        }
+    }
+
+    /// Initialise the bounding box for the mesh.
+    fn init_aabb(tris: &[SmoothTriangle]) -> Aabb {
+        let mut mins = *tris
+            .get(X as usize)
+            .unwrap()
+            .tri()
+            .verts()
+            .get(Alpha as usize)
+            .unwrap();
+        let mut maxs = mins;
+
+        for tri in tris {
+            for v in tri.tri().verts().iter() {
+                for (v, (min, max)) in v.iter().zip(mins.iter_mut().zip(maxs.iter_mut())) {
+                    if *min > *v {
+                        *min = *v;
+                    } else if *max < *v {
+                        *max = *v;
+                    }
+                }
+            }
+        }
+
+        Aabb::new(mins, maxs)
+    }
+
+    /// Calculate the total surface area.
+    #[inline]
+    #[must_use]
+    pub fn area(&self) -> f64 {
+        let mut area = 0.0;
+
+        for tri in &self.tris {
+            area += tri.tri().area();
+        }
+
+        area
+    }
+}
--- src/geom/surf/mod.rs
+++ src/geom/surf/mod.rs
@@ -1,7 +1,8 @@
 //! Surface module.
 
 pub mod aabb;
+pub mod mesh;
 pub mod smooth_triangle;
 pub mod triangle;
 
-pub use self::{aabb::*, smooth_triangle::*, triangle::*};
+pub use self::{aabb::*, mesh::*, smooth_triangle::*, triangle::*};
