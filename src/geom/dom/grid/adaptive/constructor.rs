cea23d52b70c2e62a0514860133af264853e1236
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-04-24 15:49:39 +0100

    Split up adaptive grid methods.

--- src/geom/dom/grid/adaptive/constructor.rs
+++ src/geom/dom/grid/adaptive/constructor.rs
@@ -4,42 +4,6 @@ use crate::{
     settings::Adaptive as Settings, Aabb, Collide, Group, Mesh, Pos3, Set, SmoothTriangle,
 };
 
-/// Adaptive cell enumeration.
-///
-///         6npp   7ppp
-///       4nnp   5pnp
-/// z  y    2npn   3ppn
-/// | /   0nnn   1pnn
-/// |/__x
-pub enum Adaptive<'a> {
-    /// Root cell.
-    Root {
-        /// Boundary.
-        boundary: Aabb,
-        /// Children.
-        children: [Box<Adaptive<'a>>; 8],
-    },
-    /// Branching cell.
-    Branch {
-        /// Boundary.
-        boundary: Aabb,
-        /// Children.
-        children: [Box<Adaptive<'a>>; 8],
-    },
-    /// Terminal populated cell.
-    Leaf {
-        /// Boundary.
-        boundary: Aabb,
-        /// Intersecting triangles.
-        tris: Vec<(Group, &'a SmoothTriangle)>,
-    },
-    /// Terminal empty cell.
-    Empty {
-        /// Boundary.
-        boundary: Aabb,
-    },
-}
-
 impl<'a> Adaptive<'a> {
     /// Construct a new adaptive grid.
     #[inline]
---
+++ src/geom/dom/grid/adaptive/mod.rs
@@ -0,0 +1,39 @@
+//! Adaptive gridding scheme.
+
+use crate::{Aabb, Group, SmoothTriangle};
+
+/// Adaptive cell enumeration.
+///
+///         6npp   7ppp
+///       4nnp   5pnp
+/// z  y    2npn   3ppn
+/// | /   0nnn   1pnn
+/// |/__x
+pub enum Adaptive<'a> {
+    /// Root cell.
+    Root {
+        /// Boundary.
+        boundary: Aabb,
+        /// Children.
+        children: [Box<Adaptive<'a>>; 8],
+    },
+    /// Branching cell.
+    Branch {
+        /// Boundary.
+        boundary: Aabb,
+        /// Children.
+        children: [Box<Adaptive<'a>>; 8],
+    },
+    /// Terminal populated cell.
+    Leaf {
+        /// Boundary.
+        boundary: Aabb,
+        /// Intersecting triangles.
+        tris: Vec<(Group, &'a SmoothTriangle)>,
+    },
+    /// Terminal empty cell.
+    Empty {
+        /// Boundary.
+        boundary: Aabb,
+    },
+}
