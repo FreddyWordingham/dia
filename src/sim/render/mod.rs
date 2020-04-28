55a88785aa5b4ec662b7bc88fdf18740b7675cc1
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-04-28 20:13:20 +0100

    Added image pipeline start.

--- src/sim/render/mod.rs
+++ src/sim/render/mod.rs
@@ -6,9 +6,12 @@ pub use self::camera::*;
 
 use crate::{Adaptive, Image, ParBar};
 use num_cpus;
-use palette::LinSrgba;
+use palette::{Gradient, LinSrgba};
 use rayon::prelude::*;
 use std::sync::{Arc, Mutex};
+
+/// Pixel block size.
+const BLOCK_SIZE: u64 = 128;
 
 /// Render an image.
 #[inline]
@@ -21,9 +24,11 @@ pub fn run(grid: &Adaptive, cam: &Camera) -> Image {
     let threads: Vec<usize> = (0..num_cpus::get()).collect();
     let mut images: Vec<_> = threads
         .par_iter()
-        .map(|id| run_thread(*id, grid, cam))
+        .map(|id| run_thread(*id, &Arc::clone(&pb), grid, cam))
         .collect();
-    pb.lock().unwrap().finish_with_message("Render complete");
+    pb.lock()
+        .expect("Unable to lock progress bar.")
+        .finish_with_message("Render complete");
 
     let mut base = images.pop().unwrap();
     for img in images {
@@ -36,6 +41,28 @@ pub fn run(grid: &Adaptive, cam: &Camera) -> Image {
 /// Render on a single thread.
 #[inline]
 #[must_use]
-fn run_thread(_thread_id: usize, _grid: &Adaptive, cam: &Camera) -> Image {
-    Image::from_elem(cam.sensor().res(), LinSrgba::default())
+fn run_thread(thread_id: usize, pb: &Arc<Mutex<ParBar>>, _grid: &Adaptive, cam: &Camera) -> Image {
+    let mut img = Image::from_elem(cam.sensor().res(), LinSrgba::default());
+
+    let backup = Gradient::new(vec![
+        LinSrgba::new(0.0, 0.0, 0.0, 1.0),
+        LinSrgba::new(1.0, 1.0, 1.0, 1.0),
+    ]);
+
+    let hr_res = cam.sensor().res().0;
+
+    while let Some((start, end)) = {
+        let mut pb = pb.lock().expect("Could not lock progress bar.");
+        let b = pb.block(BLOCK_SIZE);
+        std::mem::drop(pb);
+        b
+    } {
+        for n in start as usize..end as usize {
+            let pixel = (n % hr_res, n / hr_res);
+
+            img[pixel] += backup.get(thread_id as f32 * 1.0 / 8.0);
+        }
+    }
+
+    img
 }
