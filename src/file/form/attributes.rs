54ceefccdd9f7074f5870992db6edf76bc4ccfe6
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-04-29 16:18:28 +0100

    Added loadable attributes structure.

---
+++ src/file/form/attributes.rs
@@ -0,0 +1,30 @@
+//! Camera form implementation.
+
+use crate::{render::Attribute, Group, Set};
+use attr::load;
+
+/// Loadable attributes structure.
+#[load]
+pub struct Attributes {
+    /// Attribute mappings.
+    attributes: Vec<(Group, Attribute)>,
+}
+
+impl Attributes {
+    /// Build an attribute set.
+    #[inline]
+    #[must_use]
+    pub fn build(self) -> Set<Attribute> {
+        let mut attributes = Set::new();
+
+        for (group, attr) in self.attributes {
+            if attributes.contains_key(&group) {
+                panic!("Duplicate attribute for group: {}", group);
+            }
+
+            attributes.insert(group, attr);
+        }
+
+        attributes
+    }
+}
--- src/file/form/mod.rs
+++ src/file/form/mod.rs
@@ -1,7 +1,8 @@
 //! Form module.
 
+pub mod attributes;
 pub mod camera;
 pub mod colours;
 pub mod trans3;
 
-pub use self::{camera::*, colours::*, trans3::*};
+pub use self::{attributes::*, camera::*, colours::*, trans3::*};
