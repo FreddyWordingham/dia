252cfdf36cd0aa96de06989b956e0cd8c64aecc7
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-05-04 00:31:17 +0100

    Added basic shadows.

--- src/sim/render/illumination/shadow.rs
+++ src/sim/render/illumination/shadow.rs
@@ -1,10 +1,39 @@
 //! Shadowing functions.
 
-use crate::{render::Scene, Hit, Ray};
+use crate::{render::Scene, Dir3, Hit, Ray};
 
 /// Calculate the shadowing factor.
 #[inline]
 #[must_use]
-pub fn shadow(_ray: &Ray, _scene: &Scene, _hit: &Hit) -> f64 {
-    0.5
+pub fn shadow(ray: &Ray, scene: &Scene, hit: &Hit) -> f64 {
+    let mut light_ray = Ray::new(*ray.pos(), *hit.side().norm());
+    light_ray.travel(scene.sett().bump_dist());
+    let light_dir = Dir3::new_normalize(scene.sett().sun_pos() - light_ray.pos());
+    *light_ray.dir_mut() = light_dir;
+
+    let mut direct = visibility(light_ray, scene);
+
+    direct
+}
+
+/// Calculate the visibility of a given ray.
+#[inline]
+#[must_use]
+fn visibility(mut ray: Ray, scene: &Scene) -> f64 {
+    let mut vis = 1.0;
+    while let Some(hit) = scene.grid().observe(ray.clone(), scene.sett().bump_dist()) {
+        if vis <= 0.0 {
+            break;
+        }
+
+        match hit.group() {
+            _ => {
+                // Opaque
+                vis = 0.1;
+                break;
+            }
+        }
+    }
+
+    vis
 }
