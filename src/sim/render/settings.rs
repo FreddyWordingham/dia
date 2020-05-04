3cd54f79320a1f429dc658f15954e2822053aefb
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-05-04 15:35:14 +0100

    Added sun radius and soft shadow parameters to settings structure.

--- src/sim/render/settings.rs
+++ src/sim/render/settings.rs
@@ -7,19 +7,25 @@ use attr::load;
 #[load]
 #[derive(Clone)]
 pub struct Settings {
-    /// Bump distance.
+    /// Bump distance [m].
     bump_dist: f64,
     /// Sun position.
     sun_pos: Pos3,
+    /// Sun radius [m].
+    sun_rad: f64,
     /// Lighting weights.
     light_weights: [f64; 3],
     /// Specular power.
     spec_pow: i32,
+    /// Soft shadow samples.
+    soft_shadows: usize,
 }
 
 impl Settings {
     clone!(bump_dist, f64);
     access!(sun_pos, Pos3);
+    clone!(sun_rad, f64);
     access!(light_weights, [f64; 3]);
     clone!(spec_pow, i32);
+    clone!(soft_shadows, usize);
 }
