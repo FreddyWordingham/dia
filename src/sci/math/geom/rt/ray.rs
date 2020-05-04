61febca40e24ab52203a0f4471b8fbfe52a2ac58
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-05-04 15:45:56 +0100

    Added ray rotation method.

--- src/sci/math/geom/rt/ray.rs
+++ src/sci/math/geom/rt/ray.rs
@@ -1,6 +1,6 @@
 //! Ray implementation.
 
-use crate::{access, Dir3, Pos3};
+use crate::{access, Dir3, Pos3, Rot3, Vec3};
 
 /// Ray structure.
 #[derive(Clone)]
@@ -35,5 +35,24 @@ impl Ray {
         debug_assert!(dist > 0.0);
 
         self.pos += self.dir.as_ref() * dist;
+    }
+
+    /// Rotate the photon with a given pitch and subsequent roll manoeuvre.
+    #[inline]
+    #[must_use]
+    pub fn rotate(&mut self, pitch: f64, roll: f64) {
+        let arbitrary_axis = if (self.dir.z.abs() - 1.0) >= 1.0e-1 {
+            Vec3::z_axis()
+        } else {
+            Vec3::y_axis()
+        };
+
+        let pitch_axis = Dir3::new_normalize(self.dir.cross(&arbitrary_axis));
+        let pitch_rot = Rot3::from_axis_angle(&pitch_axis, pitch);
+
+        let roll_rot = Rot3::from_axis_angle(&self.dir, roll);
+
+        self.dir = roll_rot * pitch_rot * self.dir;
+        self.dir.renormalize();
     }
 }
