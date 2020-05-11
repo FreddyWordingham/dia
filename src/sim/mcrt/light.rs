cbb2f696bd9a6b84d9d5a97cf1d24e79f0670873
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-05-11 14:09:22 +0100

    Added constructor method to light mcrt structure.

--- src/sim/mcrt/light.rs
+++ src/sim/mcrt/light.rs
@@ -1,6 +1,28 @@
 //! Light module.
 
-/// Photon emission structure.
-pub struct Light {}
+use crate::{access, clone, Mesh, Spectrum};
 
-impl Light {}
+/// Photon emission structure.
+pub struct Light {
+    /// Surface.
+    surf: Mesh,
+    /// Emission spectrum.
+    spec: Spectrum,
+    /// Power [J/s].
+    power: f64,
+}
+
+impl Light {
+    access!(surf, Mesh);
+    access!(spec, Spectrum);
+    clone!(power, f64);
+
+    /// Construct a new instance.
+    #[inline]
+    #[must_use]
+    pub fn new(surf: Mesh, spec: Spectrum, power: f64) -> Self {
+        debug_assert!(power > 0.0);
+
+        Self { surf, spec, power }
+    }
+}
