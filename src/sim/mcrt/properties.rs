64cfd738da2d2eb23ce8b08a23041f88ffa627fb
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-05-11 18:43:42 +0100

    Added optical properties and environment structures.

--- src/sim/mcrt/properties.rs
+++ src/sim/mcrt/properties.rs
@@ -1,22 +1,50 @@
 //! Light module.
 
-use crate::{access, Formula, Range};
+use crate::{access, mcrt::Environment, Formula};
 
 /// Physical properties structure.
 pub struct Properties {
+    /// Refractive index.
+    ref_index: Formula,
     /// Scattering coefficient [1/m].
     scat_coeff: Formula,
     /// Absorption coefficient [1/m].
     abs_coeff: Option<Formula>,
     /// Shifting coefficient [1/m].
     shift_coeff: Option<Formula>,
-    /// Valid wavelength range.
-    range: Range,
+    /// Asymmetry factor.
+    asym_fact: Formula,
 }
 
 impl Properties {
+    access!(ref_index, Formula);
     access!(scat_coeff, Formula);
     access!(abs_coeff, Option<Formula>);
     access!(shift_coeff, Option<Formula>);
-    access!(range, Range);
+    access!(asym_fact, Formula);
+
+    /// Generate an optical environment for a given wavelength.
+    #[inline]
+    #[must_use]
+    pub fn env(&self, w: f64) -> Environment {
+        let index = self.ref_index.y(w);
+
+        let scat = self.scat_coeff.y(w);
+
+        let abs = if let Some(abs_coeff_formula) = &self.abs_coeff {
+            abs_coeff_formula.y(w)
+        } else {
+            0.0
+        };
+
+        let shift = if let Some(shift_coeff_formula) = &self.shift_coeff {
+            shift_coeff_formula.y(w)
+        } else {
+            0.0
+        };
+
+        let g = self.asym_fact.y(w);
+
+        Environment::new(index, scat, abs, shift, g)
+    }
 }
