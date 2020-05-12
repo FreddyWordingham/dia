9277e35e8d82b0efc99b04cca79816a312d274b9
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-05-12 19:46:17 +0100

    Added build_from_list set function.B

--- src/tools/ord/set.rs
+++ src/tools/ord/set.rs
@@ -1,15 +1,16 @@
 //! Set implementation.
 
-use crate::Group;
+use crate::{Build, Error, Group};
 use std::collections::BTreeMap;
+use std::path::Path;
 
 /// Set alias.
 pub type Set<T> = BTreeMap<Group, T>;
 
-/// Build a set from a list.
+/// Convert a set from a list.
 #[inline]
 #[must_use]
-pub fn set_from_list<T>(list: Vec<(Group, T)>) -> Set<T> {
+pub fn convert_from_list<T>(list: Vec<(Group, T)>) -> Set<T> {
     let mut set = Set::new();
 
     for (group, item) in list {
@@ -21,4 +22,25 @@ pub fn set_from_list<T>(list: Vec<(Group, T)>) -> Set<T> {
     }
 
     set
+}
+
+/// Build a set from a list.
+/// # Errors
+/// if an item could not be built.
+#[inline]
+pub fn build_from_list<T: Build>(
+    list: Vec<(Group, T)>,
+    in_dir: &Path,
+) -> Result<Set<T::Inst>, Error> {
+    let mut set = Set::new();
+
+    for (group, item) in list {
+        if set.contains_key(&group) {
+            panic!("Duplicate entries for group: {}", group);
+        }
+
+        set.insert(group, item.build(in_dir)?);
+    }
+
+    Ok(set)
 }
