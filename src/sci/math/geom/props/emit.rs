c84535503a8d958f7904b48fa3dcdeff5b509932
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-05-19 17:47:32 +0100

    Added emit implementation for pos3.

--- src/sci/math/geom/props/emit.rs
+++ src/sci/math/geom/props/emit.rs
@@ -1,10 +1,30 @@
 //! Emit trait.
 
-use rand::rngs::ThreadRng;
+use crate::{Dir3, Pos3, Ray, Vec3};
+use rand::{rngs::ThreadRng, Rng};
+use std::f64::consts::PI;
 
 /// Emit trait implementation.
-/// Types implementing this trait can generate Rays.
+/// Types implementing this trait can cast Rays.
 pub trait Emit {
-    /// Check for an overlapping collision.
-    fn emit(&self, rng: &mut ThreadRng) -> bool;
+    /// Cast a new ray.
+    fn cast(&self, rng: &mut ThreadRng) -> Ray;
+}
+
+impl Emit for Pos3 {
+    #[inline]
+    #[must_use]
+    fn cast(&self, rng: &mut ThreadRng) -> Ray {
+        let theta = rng.gen_range(0.0, 2.0 * PI);
+        let z = rng.gen_range(-1.0, 1.0);
+
+        Ray::new(
+            *self,
+            Dir3::new_normalize(Vec3::new(
+                (1.0_f64 - (z * z)).sqrt() * theta.cos(),
+                (1.0_f64 - (z * z)).sqrt() * theta.sin(),
+                z,
+            )),
+        )
+    }
 }
