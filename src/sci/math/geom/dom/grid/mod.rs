284df6c8c14c2c63e502d4a054c7139405b18eae
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-05-24 19:33:39 +0100

    Added gen_index_voxel methods to grid structure.

--- src/sci/math/geom/dom/grid/mod.rs
+++ src/sci/math/geom/dom/grid/mod.rs
@@ -4,7 +4,7 @@ pub mod settings;
 
 pub use self::settings::*;
 
-use crate::{access, report, Aabb, Error, Vec3, X, Y, Z};
+use crate::{access, report, Aabb, Error, Pos3, Vec3, X, Y, Z};
 use std::fmt::{Display, Formatter};
 
 /// Regular grid structure.
@@ -44,6 +44,41 @@ impl Grid {
     #[must_use]
     pub const fn total_cells(&self) -> usize {
         self.res[X] * self.res[Y] * self.res[Z]
+    }
+
+    /// If the given position is contained within the grid,
+    /// generate the index for the given position within the grid.
+    #[inline]
+    #[must_use]
+    pub fn gen_index(&self, p: &Pos3) -> Option<[usize; 3]> {
+        if self.bound.contains(p) {
+            let mins = self.bound.mins();
+            let maxs = self.bound.maxs();
+            Some([
+                (((p.x - mins.x) / (maxs.x - mins.x)) * self.res[X] as f64).floor() as usize,
+                (((p.y - mins.y) / (maxs.y - mins.y)) * self.res[Y] as f64).floor() as usize,
+                (((p.z - mins.z) / (maxs.z - mins.z)) * self.res[Z] as f64).floor() as usize,
+            ])
+        } else {
+            None
+        }
+    }
+
+    /// If the given position is contained within the grid,
+    /// generate the index and voxel for the given position within the grid.
+    #[inline]
+    #[must_use]
+    pub fn gen_index_voxel(&self, p: &Pos3) -> Option<([usize; 3], Aabb)> {
+        if let Some(index) = self.gen_index(p) {
+            let mut min = *self.bound.mins();
+            min.x += self.voxel_size[X] * index[X] as f64;
+            min.y += self.voxel_size[Y] * index[Y] as f64;
+            min.x += self.voxel_size[Z] * index[Z] as f64;
+
+            Some((index, Aabb::new(min, min + self.voxel_size)))
+        } else {
+            None
+        }
     }
 }
 
