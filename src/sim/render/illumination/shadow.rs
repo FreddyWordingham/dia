ca9761a9c59a3dea6ca468edc59739f788ccfa5a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-06-06 13:02:46 +0100

    Added tree render test.

--- src/sim/render/illumination/shadow.rs
+++ src/sim/render/illumination/shadow.rs
@@ -4,9 +4,11 @@ use crate::{golden, render::Input, Dir3, Hit, Ray};
 use rand::{rngs::ThreadRng, Rng};
 use std::f64::consts::PI;
 
+/// Maximum distance tested for ray visibility [m].
 const MAX_VISIBILITY_DIST: f64 = 1.0e9;
+
+/// Ambient occlusion power constant.
 const AMBIENT_OCCLUSION_POWER: i32 = 8;
-const DEFAULT_AMBIENT: f64 = 0.7;
 
 /// Calculate the shadowing factor.
 #[inline]
@@ -27,16 +29,29 @@ pub fn shadow(input: &Input, ray: &Ray, hit: &Hit, bump_dist: f64, rng: &mut Thr
         }
         (total / f64::from(samples)).powi(AMBIENT_OCCLUSION_POWER)
     } else {
-        DEFAULT_AMBIENT
+        1.0
     };
 
     let sun_dir = Dir3::new_normalize(input.sett.sun_pos() - ray.pos());
     let mut light_ray = Ray::new(*ray.pos(), *hit.side().norm());
     light_ray.travel(bump_dist);
     *light_ray.dir_mut() = sun_dir;
-    let solar = visibility(input, light_ray, bump_dist);
 
-    ambient + solar
+    let solar = if let Some(samples) = input.sett.soft_shadows() {
+        let offset = rng.gen_range(0.0, 2.0 * PI);
+        let mut total = 0.0;
+        for n in 0..samples {
+            let (r, theta) = golden::circle(n, samples);
+            let mut soft_ray = light_ray.clone();
+            soft_ray.rotate(r * input.sett.sun_rad(), theta + offset);
+            total += visibility(input, soft_ray, bump_dist);
+        }
+        total / f64::from(samples)
+    } else {
+        visibility(input, light_ray, bump_dist)
+    };
+
+    ambient * solar
 }
 
 /// Calculate the visibility of a given ray.
--- src/sim/render/settings.rs
+++ src/sim/render/settings.rs
@@ -13,15 +13,21 @@ pub struct Settings {
     bump_dist: f64,
     /// Sun position [m].
     sun_pos: Pos3,
+    /// Sun radius [rad].
+    sun_rad: f64,
     /// Optional number of ambient occlusion samples.
     ambient_occlusion: Option<i32>,
+    /// Optional number of soft shadow samples.
+    soft_shadows: Option<i32>,
 }
 
 impl Settings {
     clone!(block_size, u64);
     clone!(bump_dist, f64);
+    clone!(sun_rad, f64);
     access!(sun_pos, Pos3);
     clone!(ambient_occlusion, Option<i32>);
+    clone!(soft_shadows, Option<i32>);
 }
 
 impl Display for Settings {
@@ -31,10 +37,16 @@ impl Display for Settings {
         display_field_ln!(fmt, "block size", self.block_size)?;
         display_field_ln!(fmt, "bump distance", self.bump_dist, "m")?;
         display_field_ln!(fmt, "sun position", self.sun_pos, "m")?;
+        display_field_ln!(fmt, "sun radius", self.sun_rad.to_degrees(), "deg")?;
         if let Some(samples) = self.ambient_occlusion {
-            display_field!(fmt, "ambient occlusion samples", samples)
+            display_field_ln!(fmt, "ambient occlusion samples", samples)?;
         } else {
-            display_field!(fmt, "ambient occlusion", "OFF")
+            display_field_ln!(fmt, "ambient occlusion", "OFF")?;
+        }
+        if let Some(samples) = self.soft_shadows {
+            display_field!(fmt, "soft shadow samples", samples)
+        } else {
+            display_field!(fmt, "soft shadows", "OFF")
         }
     }
 }
