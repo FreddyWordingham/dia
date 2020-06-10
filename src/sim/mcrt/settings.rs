789012b98c68bba79727ed0d701e5ec310f0f01e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-06-10 17:52:16 +0100

    Added initial material variable to mcrt settings.

--- src/sim/mcrt/settings.rs
+++ src/sim/mcrt/settings.rs
@@ -1,6 +1,6 @@
 //! Settings implementation.
 
-use crate::{clone, display_field, display_field_ln};
+use crate::{access, clone, display_field, display_field_ln, Group};
 use attr::load;
 use std::fmt::{Display, Formatter, Result};
 
@@ -13,12 +13,15 @@ pub struct Settings {
     num_phot: u64,
     /// Bump distance [m].
     bump_dist: f64,
+    /// Initial material.
+    init_mat: Group,
 }
 
 impl Settings {
     clone!(block_size, u64);
     clone!(num_phot, u64);
     clone!(bump_dist, f64);
+    access!(init_mat, Group);
 }
 
 impl Display for Settings {
@@ -27,6 +30,7 @@ impl Display for Settings {
     fn fmt(&self, fmt: &mut Formatter) -> Result {
         display_field_ln!(fmt, "block size", self.block_size)?;
         display_field_ln!(fmt, "number of photons", self.num_phot)?;
-        display_field!(fmt, "bump distance", self.bump_dist, "m")
+        display_field_ln!(fmt, "bump distance", self.bump_dist, "m")?;
+        display_field!(fmt, "initial material", &self.init_mat)
     }
 }
