7e61b951344468d4c9292b3b0efc90d29485988f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-06-24 14:05:49 +0100

    Added points light.

--- src/sim/mcrt/light.rs
+++ src/sim/mcrt/light.rs
@@ -1,33 +1,119 @@
 //! Light structure.
 
-use crate::{
-    access, clone, display_field, display_field_ln, mcrt::Photon, Emit, Mesh, Probability,
-};
-use rand::rngs::ThreadRng;
+use crate::{display_field, display_field_ln, mcrt::Photon, Emit, Mesh, Pos3, Probability, Ray};
+use ndarray::Array1;
+use rand::{rngs::ThreadRng, Rng};
 use std::fmt::{Display, Formatter, Result};
 
-/// Photon emission structure.
-pub struct Light {
-    /// Surface.
-    surf: Mesh,
-    /// Emission Formula.
-    spec: Probability,
-    /// Power [J/s].
-    power: f64,
+/// Photon emission enumeration.
+pub enum Light {
+    /// Surface type
+    Surface {
+        /// Surface.
+        surf: Mesh,
+        /// Emission Formula.
+        spec: Probability,
+        /// Power [J/s].
+        power: f64,
+    },
+    /// Points type.
+    Points {
+        /// Weighted emission points.
+        points: Array1<(Pos3, f64)>,
+        /// Power [J/s].
+        power: f64,
+        /// Emission Formula.
+        spec: Probability,
+    },
 }
 
 impl Light {
-    access!(surf, Mesh);
-    access!(spec, Probability);
-    clone!(power, f64);
-
-    /// Construct a new instance.
+    /// Construct a new surface instance.
     #[inline]
     #[must_use]
     pub fn new(surf: Mesh, spec: Probability, power: f64) -> Self {
         debug_assert!(power > 0.0);
 
-        Self { surf, spec, power }
+        Self::Surface { surf, spec, power }
+    }
+
+    /// Reference the light's surface mesh.
+    #[inline]
+    #[must_use]
+    pub fn surf(&self) -> &Mesh {
+        match self {
+            Self::Surface {
+                surf,
+                spec: _,
+                power: _,
+            } => surf,
+            Self::Points { .. } => {
+                panic!("No surface.");
+            }
+        }
+    }
+
+    /// Reference the light's emission formula.
+    #[inline]
+    #[must_use]
+    pub fn spec(&self) -> &Probability {
+        match self {
+            Self::Surface {
+                surf: _,
+                spec,
+                power: _,
+            } => spec,
+            Self::Points {
+                points: _,
+                power: _,
+                spec,
+            } => spec,
+        }
+    }
+
+    /// Reference the light's power.
+    #[inline]
+    #[must_use]
+    pub fn power(&self) -> f64 {
+        match self {
+            Self::Surface {
+                surf: _,
+                spec: _,
+                power,
+            } => *power,
+            Self::Points {
+                points: _,
+                power,
+                spec: _,
+            } => *power,
+        }
+    }
+
+    /// Generate a ray.
+    #[inline]
+    #[must_use]
+    pub fn gen_ray(&self, rng: &mut ThreadRng) -> Ray {
+        match self {
+            Self::Surface {
+                surf,
+                spec: _,
+                power: _,
+            } => surf.cast(rng),
+            Self::Points {
+                points,
+                power: _,
+                spec: _,
+            } => {
+                let r = rng.gen::<f64>();
+                for (p, x) in points {
+                    if r <= *x {
+                        let dir = crate::Vec3::x_axis(); // TODO: Make random.
+                        return Ray::new(*p, dir);
+                    }
+                }
+                panic!("Invalid point weightings.");
+            }
+        }
     }
 
     /// Emit a photon.
@@ -36,9 +122,10 @@ impl Light {
     pub fn emit(&self, total_phot: u64, rng: &mut ThreadRng) -> Photon {
         debug_assert!(total_phot > 0);
 
-        let ray = self.surf.cast(rng);
-        let wavelength = self.spec.gen(rng);
-        let power = self.power / total_phot as f64;
+        let ray = self.gen_ray(rng);
+
+        let wavelength = self.spec().gen(rng);
+        let power = self.power() / total_phot as f64;
 
         Photon::new(ray, wavelength, power)
     }
@@ -48,8 +135,15 @@ impl Display for Light {
     #[allow(clippy::result_expect_used)]
     #[inline]
     fn fmt(&self, fmt: &mut Formatter) -> Result {
-        display_field_ln!(fmt, "number of triangles", self.surf.tris().len())?;
-        display_field_ln!(fmt, "formula", &self.spec)?;
-        display_field!(fmt, "power", self.power, "J/s")
+        match self {
+            Self::Surface { .. } => {
+                display_field_ln!(fmt, "type", "Emission surface")?;
+            }
+            Self::Points { .. } => {
+                display_field_ln!(fmt, "type", "Point array")?;
+            }
+        }
+        display_field_ln!(fmt, "formula", self.spec())?;
+        display_field!(fmt, "power", self.power(), "J/s")
     }
 }
