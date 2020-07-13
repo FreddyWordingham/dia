93dae9f140ddf2f1c3c01f8d6b231ca9a28d5f0f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-07-13 02:35:14 +0100

    Added settings setup structure.

--- src/sim/render/setup/mod.rs
+++ src/sim/render/setup/mod.rs
@@ -4,5 +4,6 @@ pub mod engine;
 pub mod lighting;
 pub mod order;
 pub mod scene;
+pub mod settings;
 
-pub use self::{engine::*, lighting::*, order::*, scene::*};
+pub use self::{engine::*, lighting::*, order::*, scene::*, settings::*};
